c4c40db9a9fb2e23901531610dc2d2293abd20b2
tiendatnt <good.engineer810@gmail.com>
2019-02-07 18:56:59 +0900

    bai_2_1: structure of char device driver

---
+++ phan_2/bai_2_1/vchar_driver.c
@@ -0,0 +1,80 @@
+/*
+ * ten file: vchar_driver.c
+ * tac gia : dat.a3cbq91@gmail.com
+ * ngay tao: 9/12/2018
+ * mo ta   : char driver cho thiet bi gia lap vchar_device.
+ *           vchar_device la mot thiet bi nam tren RAM.
+ */
+
+#include <linux/module.h> /* thu vien nay dinh nghia cac macro nhu module_init va module_exit */
+
+#define DRIVER_AUTHOR "Nguyen Tien Dat <dat.a3cbq91@gmail.com>"
+#define DRIVER_DESC   "A sample character device driver"
+#define DRIVER_VERSION "0.1"
+
+/****************************** device specific - START *****************************/
+/* ham khoi tao thiet bi */
+
+/* ham giai phong thiet bi */
+
+/* ham doc tu cac thanh ghi du lieu cua thiet bi */
+
+/* ham ghi vao cac thanh ghi du lieu cua thiet bi */
+
+/* ham doc tu cac thanh ghi trang thai cua thiet bi */
+
+/* ham ghi vao cac thanh ghi dieu khien cua thiet bi */
+
+/* ham xu ly tin hieu ngat gui tu thiet bi */
+
+/******************************* device specific - END *****************************/
+
+/******************************** OS specific - START *******************************/
+/* cac ham entry points */
+
+/* ham khoi tao driver */
+static int __init vchar_driver_init(void)
+{
+	/* cap phat device number */
+
+	/* tao device file */
+
+	/* cap phat bo nho cho cac cau truc du lieu cua driver va khoi tao */
+
+	/* khoi tao thiet bi vat ly */
+
+	/* dang ky cac entry point voi kernel */
+
+	/* dang ky ham xu ly ngat */
+
+	printk("Initialize vchar driver successfully\n");
+	return 0;
+}
+
+/* ham ket thuc driver */
+static void __exit vchar_driver_exit(void)
+{
+	/* huy dang ky xu ly ngat */
+
+	/* huy dang ky entry point voi kernel */
+
+	/* giai phong thiet bi vat ly */
+
+	/* giai phong bo nho da cap phat cau truc du lieu cua driver */
+
+	/* xoa bo device file */
+
+	/* giai phong device number */
+
+	printk("Exit vchar driver\n");
+}
+/********************************* OS specific - END ********************************/
+
+module_init(vchar_driver_init);
+module_exit(vchar_driver_exit);
+
+MODULE_LICENSE("GPL"); /* giay phep su dung cua module */
+MODULE_AUTHOR(DRIVER_AUTHOR); /* tac gia cua module */
+MODULE_DESCRIPTION(DRIVER_DESC); /* mo ta chuc nang cua module */
+MODULE_VERSION(DRIVER_VERSION); /* mo ta phien ban cuar module */
+MODULE_SUPPORTED_DEVICE("testdevice"); /* kieu device ma module ho tro */
